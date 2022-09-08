811ef2a28e2b258652954c62d8e44228ad6244d2
Henrique Rocha <henrique.rocha@gmail.com>
2022-09-08 00:35:38 -0400

    Adding support for SQLite Pods update

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+var SQLite = require('react-native-sqlite-storage')
+
 AppRegistry.registerComponent(appName, () => App);
+
+//var db = SQLite.openDatabase("test.db", "1.0", "Test Database");
